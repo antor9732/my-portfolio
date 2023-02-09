873e8311bc13267036dfdefc20abf374f72ae138
MD. Mahmudul  Hasan <mahmudul.cse47@gmail.com>
2023-02-09 18:59:48 +0600

    update file

--- resources/views/Portfolios/index.blade.php
+++ resources/views/Portfolios/index.blade.php
@@ -39,7 +39,7 @@
         <header class="masthead">
             <div class="container">
                 <div class="masthead-subheading">Welcome To My Website!</div>
-                <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
+                <div class="masthead-heading text-uppercase">This Is Md Mahmudul Hasan</div>
                 <a class="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
             </div>
         </header>
--- resources/views/Portfolios/main.blade.php
+++ resources/views/Portfolios/main.blade.php
@@ -8,7 +8,29 @@
             <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
             <li class="breadcrumb-item active"><a href="{{ route('admin.dashboard') }}">Main</a></li>
         </ol>
-<H1>Hello I am Md Mahmudul Hasan</H1>
+
+        
+        <div class="row">
+          <div class="form-group col-md-3 mt-3">
+            <h3>Background Image</h3>
+            <img style="height: 30vh" class="img-thumbnail" src="{{asset('assets/img/header-bg.jpg')}}" alt="background-image">
+          <input class="mt-3" type="file" id="bg-img" name="bg-img">
+          </div>
+          <div class="form-group col-md-4 mt-3">
+            <div>
+              <label for="title"><H4>Title</H4></label>
+              <input type="text" class="form-control" id="title" name="title">
+            </div>
+            <div class="mt-2 mb-4">
+              <label for="sub_title"><H4>Sub Title</H4></label>
+              <input type="text" class="form-control" id="sub_title" name="sub_title">
+            </div>
+            <div class="mt-2">
+              <H4>Upload Resume</H4>
+              <input type="file" id="resume" name="resume">
+            </div>
+          </div>
+        </div>
     </div>
   </main>
 
